12a2b76f1702d0be2f44c6db7514e359a0844996
Sarah <sarah.mattar@mac.com>
2019-12-23 14:47:59 -0800

    prepare for deployment

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-
 const connectDb = require('./config/db');
-
+const path = require('path');
 //Connect Database
 connectDb();
 
@@ -16,6 +15,16 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	//set static folder
+	app.use(express.static('/client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.get('/', (req, res) => {
 	res.json({ msg: 'Welcome to the Contact Keeper API!' });
