1dcd85a17d154697aa8a18054858fc2152d4cd39
Sarah <sarah.mattar@mac.com>
2019-12-23 15:19:40 -0800

    fixed express static folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDb = require('./config/db');
 const path = require('path');
+
 //Connect Database
 connectDb();
 
@@ -19,16 +20,11 @@ app.use('/api/contacts', require('./routes/contacts'));
 //Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
 	//set static folder
-	app.use(express.static('/client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
 }
-
-app.get('/', (req, res) => {
-	res.json({ msg: 'Welcome to the Contact Keeper API!' });
-});
 
 app.listen(PORT, () => {
 	console.log(`Server is up on port ${PORT}.`);
